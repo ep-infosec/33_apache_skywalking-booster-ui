207f07edc28d692c02b2625068d811be905d9d39
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-12 05:59:29 +0200

    init

---
+++ src/router/data/aws.ts
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default [
+  {
+    path: "",
+    name: "AWSCloud",
+    meta: {
+      title: "AWSCloud",
+      icon: "cloud_queue",
+      hasGroup: true,
+    },
+    redirect: "/aws-eks",
+    children: [
+      {
+        path: "/aws-eks",
+        name: "AWSCloudEKS",
+        meta: {
+          title: "AWSCloudEKS",
+          layer: "AWS_EKS",
+        },
+      },
+      {
+        path: "/aws-eks/tab/:activeTabIndex",
+        name: "EKSActiveTabIndex",
+        meta: {
+          notShow: true,
+          layer: "AWS_EKS",
+        },
+      },
+    ],
+  },
+];
